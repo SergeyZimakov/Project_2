93f62f4090b19a0e05fd86517a4ef93afa65516f
Sergey Zimakov <zimakovs93@gmail.com>
2020-09-26 19:33:13 +0300

    complete without bonus

--- js/main.js
+++ js/main.js
@@ -1,20 +1,22 @@
 ///////////////////////////////////////////////////////////////////////
-///Descriptions of some functions is above of function declarations///
+///Description of some functions is above of function declarations///
 /////////////////////////////////////////////////////////////////////
 
 
 
 //////state////////
-const itemsToCopyToState = 20;
+///-setting global variables takes up place in memory, but allows easy access if needed-///
+const firstIndexToCopyToState = 0;
+const lastIndexToCopyToState = 50;
+const limitOfFavorites = 5;
+const timeToRefreshMoreInfo = 120000; // 120000 ms = 2 min
 let state = {
     coins: [],
     favoritesList: [],
-    forReplace: {
-        indexToReplace: '',
-        coinToAdd: '',
-        coinToRemove: ''
-    }
+    indexOfSelectedCoin: ''
 }
+
+let cache = {};
 
 function start() {
     requestForCoins();
@@ -36,8 +38,14 @@ function about() {
     $('#about').show();
 }
 
-function cloneItemsFromArrayToState(arr, numberOfItems) {
-    for (let i = 0; i < numberOfItems; i ++) {
+function cloneItemsFromArrayToState(arr, firstIndex, lastIndex) {
+    for (let i = firstIndex; i < lastIndex; i ++) {
+        if(arr[i].id === arr[i].symbol) {
+            arr[i].symbol += '_';
+        }
+        else if (arr[i].id === arr[i].name) {
+            arr[i].name += '__';
+        }
         state.coins[i] = arr[i];
     }
 }
@@ -50,13 +58,13 @@ function requestForCoins() {
         url: 'https://api.coingecko.com/api/v3/coins/list',
         ////display loader until response is ready////////
         beforeSend: function() {
-            $('#data').html(loader());
+            $(`#data`).html(loader());
         },
         success: function(response) {
-            // state.coins = [...response];
-            cloneItemsFromArrayToState(response, itemsToCopyToState);
+            state.coins = [...response];
+            // cloneItemsFromArrayToState(response, firstIndexToCopyToState, lastIndexToCopyToState);//using for facilitating
             $('#data').html(createCards());
-            console.log(response);
+            console.log(state.coins);
         },
         error: function() {
             console.log('error');
@@ -75,7 +83,6 @@ function loader() {
 //////////////////////////////
 
 function createCards() {
-    console.log(state.coins);
     $('#data').empty();
     let innerHtml = '';
     state.coins.forEach((coin, index) => innerHtml += createSingleCard(coin, index));
@@ -90,10 +97,8 @@ function createSingleCard(coin, index) {
             <div class="card-body">
                 <h5 class="card-title">${coin.symbol}</h5>
                 <p class="card-text">${coin.id}</p>
-                <div class="collapseMoreInfo" id="collapseMoreInfo">
-                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
-                </div>
-                <button class="btn btn-primary" onclick="collapseMoreInfo(${index})">More info</button>
+                <div class="collapseMoreInfo" id="${coin.id}"></div>
+                <button class="btn btn-primary" onclick="collapseMoreInfo(event, ${index})">More info</button>
             </div>
         </div>
     </div>`);
@@ -103,65 +108,72 @@ function createSingleCard(coin, index) {
 function updateFavoritesList(e, index) {
     const toggleBtn = e.target;
     if (toggleBtn.checked === true) { ///add to Favorites
-        if (state.favoritesList.length < 5) {
-            state.favoritesList.push(state.coins[index].name);
-        }
-        else {
-            console.log('openModal');
-            state.forReplace.coinToAdd = state.coins[index].name;
-            openModalWindow();
-
-
-        }
+        state.favoritesList.push({
+            coin: state.coins[index].symbol,
+            toggleButtonId: toggleBtn.id
+        });
+        console.log(state.favoritesList);
     }
     else { ///remove from favorites
-        state.favoritesList = state.favoritesList.filter((item, i) => i != index);
+        state.favoritesList = state.favoritesList.filter((item, i) => item.coin != state.coins[index].symbol);
+        console.log(state.favoritesList);
+    } 
+    if (state.favoritesList.length > limitOfFavorites) { ///out of limit
+        openModalWindow();
         console.log(state.favoritesList);
     }
 }
 
-///open modal window and fill it///
+///open and fill the modal window///
 function openModalWindow() {
     createModalList();
     $('#modal').show();
 }
 
-function selectCoinToReplace(e) {
-    state.forReplace.coinToRemove = e.target.id;
-    createModalList();
-    document.getElementById(state.forReplace.coinToRemove).className = 'modal-list-item toRemove';
-}
-
  function createModalList() {
     $('#modal-list').empty();
     let list = '';
-    state.favoritesList.forEach((item) => list +=  (`<div id="${item}" class="modal-list-item" onclick="selectCoinToReplace(event)">${item}</div>`))
+    state.favoritesList.forEach(
+        function(item, index) {
+            if (index < limitOfFavorites) {///last member of array is going to be added(maybe), but still not in list
+                list +=  (`<div id="${index}" class="modal-list-item" onclick="selectCoinToReplace(event)">${item.coin}</div>`);
+            }
+    });
     $('#modal-list').html(list);
  }
 
- 
+ function selectCoinToReplace(e) {
+    state.indexOfSelectedCoin = e.target.id;
+    createModalList();
+    document.getElementById(e.target.id).className = 'modal-list-item toRemove';//add line-throw
+} 
 
- function replaceCoinsInFavorites() {
-    let indexToRemove = state.favoritesList.indexOf(state.forReplace.coinToRemove);
-    state.favoritesList[indexToRemove] = state.forReplace.coinToAdd;
+///this func runs when user clicks "Save" in modal
+ function removeSelectedCoinFromFavorites() {
+    if (!state.indexOfSelectedCoin) { //if user did not mark any coin to remove
+        abortChangesInFavorites();//"Save" button will work like "Cancel"
+        return;
+    }
+    let toggleBtnToSwitchOf = document.getElementById(state.favoritesList[state.indexOfSelectedCoin].toggleButtonId);
+    toggleBtnToSwitchOf.checked = false;
+    state.favoritesList = state.favoritesList.filter((item, index) => index != state.indexOfSelectedCoin)
     $('#modal').hide();
-    // document.getElementById(stateForReplace.itemToAdd).firstElementChild.checked = true;
-    // document.getElementById(stateForReplace.itemToRemove).firstElementChild.checked = false;
-    
+    console.log(state.favoritesList);
  }
 
- ///hiding a modal if no changes were made(pressed Cancel)
- function hideModal() {
-    $('#modal').hide();
-    document.getElementById(stateForReplace.itemToAdd).firstElementChild.checked = false;
+ ///hiding a modal if no changes were made(Cancel button)
+ function abortChangesInFavorites() {
+     let toggleBtnToSwitchOf = document.getElementById(state.favoritesList[limitOfFavorites].toggleButtonId);
+     toggleBtnToSwitchOf.checked = false;
+     state.favoritesList.pop();
+     $('#modal').hide();
+     console.log(state.favoritesList);
  }
 
  function updateSearchItem() {
-     let searchItem = $('#searchItem').val();
-     if (searchItem === '') {///show again all cards after search if input field is empty
-        let cards = $('#data').children().children();
-        for (item of cards) {
-            item.style.display = 'block';
+     if ($('#searchItem').val() === '') {///show again all cards after search if input field is empty
+        for (card of $('#data').children()) {
+            card.style.display = 'block';
         }
      }
  }
@@ -169,31 +181,95 @@ function selectCoinToReplace(e) {
  function search() {
     home();
     let searchItem = $('#searchItem').val();
+    searchItem = searchItem.toLowerCase();
     if (searchItem === '') {
         alert('Enter a code of crypto currency please.');
     }
     else {
-        let cards = $('#data').children().children();
-        for (let i = 0; i < favoritesList.length; i ++) {
-            if (searchItem === favoritesList[i]) {
-                for (item of cards) {///hiding all cards
-                    item.style.display = 'none';
+        for (let i = 0; i < state.coins.length; i ++) {
+            if (state.coins[i].symbol === searchItem) {
+                for (card of $('#data').children()) {///hiding all cards
+                    card.style.display = 'none';
                     }
-                document.getElementById(searchItem).parentElement.style.display = 'block';///showing search result
+                document.getElementById(searchItem).style.display = 'block';///show search result
                 return;///stop the func if we found the item
             }
         }
-        alert('This Crypto Currenny did not found!\nMaybe it does not exist in your favorits list or code is incorrect.\nTry again please.');
+        alert('This Crypto Currenny did not found!\nMaybe it does not appear in your favorits list or code is incorrect.\nTry again please.');
         //getting here if the item was not found
     }
  }
 
-
- function collapseMoreInfo(event) {
-     let btn = $(event.target);
-     btn.prev().toggle();
-     
-     
+///show or hide more information about coin
+ function collapseMoreInfo(event, index) {
+     let moreInfoDiv = document.getElementById(state.coins[index].id);
+     let btn = event.target;
+     btn.innerHTML = btn.innerHTML === 'More info' ? 'Hide': 'More info';
+     moreInfoDiv.style.display = moreInfoDiv.style.display === 'block' ? 'none' : 'block';
+     if (moreInfoDiv.style.display === 'block') {
+        createMoreInfoInnerHtml(state.coins[index].id);
+    }   
  }
 
- 
+ function createMoreInfoInnerHtml(id) {
+     let html = '';
+     if (!cache[id] || InfoIsOutOfTime(cache[id])) {//if no info in cache or info is outdated
+        $.ajax({
+            type: 'GET',
+            url: `https://api.coingecko.com/api/v3/coins/${id}`,
+            // display loader until response is ready////////
+            beforeSend: function() {
+                document.getElementById(id).innerHTML = loader();
+            },
+            success: function(response) {
+                console.log('responseMoreInfo:', response);
+                cache[id] = {
+                    usd: response.market_data.current_price.usd ? `$ ${response.market_data.current_price.usd}` : 'no information',
+                    eur: response.market_data.current_price.eur ? `€ ${response.market_data.current_price.eur}` : 'no information', 
+                    ils: response.market_data.current_price.ils ? `₪ ${response.market_data.current_price.ils}` : 'no information',
+                    lastRequest: new Date,
+                    img: response.image.small
+                }
+                console.log('cahce after response:', cache);
+                document.getElementById(id).innerHTML = moreInfoToHtml(cache[id]);
+            },
+            error: function() {
+                console.log('error');
+            }
+        });
+        }
+        else {
+            document.getElementById(id).innerHTML = moreInfoToHtml(cache[id]);
+            
+     }
+     console.log('html in createMoreInfo', html);
+ }
+
+
+
+ function moreInfoToHtml(obj) {
+     console.log('moreInfoToHtml obj:', obj)
+     return (
+        `<img src="${obj.img}">
+         <table>
+         <tr>
+             <td>USD:</td>
+             <td>${obj.usd}</td>
+         </tr>
+         <tr>
+             <td>EURO:</td>
+             <td>${obj.eur}</td>
+         </tr>
+         <tr>
+             <td>ILS:</td>
+             <td>${obj.ils}</td>
+         </tr>
+     </table>`
+     )
+ }
+
+ function InfoIsOutOfTime(obj) {
+     let newRequest = new Date;
+     let passedTime = newRequest - obj.lastRequest;
+     return passedTime > timeToRefreshMoreInfo;
+ }
